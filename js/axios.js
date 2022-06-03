38308ae98efcb349b0260e59625bd86b477ef1ab
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-03 17:39:08 -0300

    Update axios.js
    Imported secrets.js file

--- js/axios.js
+++ js/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-import secrets from "../secrets.json";
+import * as secrets from "../secrets.json";
+
 const instance = axios.create({
     baseURL: 'https://deezerdevs-deezer.p.rapidapi.com/',
     headers: {
-        'X-RapidAPI-Host': secrets.XRapidAPIHost,
-        'X-RapidAPI-Key': secrets.XRapidAPIKey
+        'X-RapidAPI-Host': secrets.secrets.XRapidAPIHost,
+        'X-RapidAPI-Key': secrets.secrets.XRapidAPIKey
     }
 });
 
