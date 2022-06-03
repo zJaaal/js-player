0bef278eca275975118b1d16f94a9aa4c9c9961a
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-03 17:14:33 -0300

    Update right-aside.js
    Added response to empty list. Someone might be really bored to do all the clicks to get there.

--- js/right-aside.js
+++ js/right-aside.js
@@ -1,3 +1,5 @@
+import { getTruncFans } from "./utils";
+
 export function drawRightAside(artists, list){
 
     // Event listener
@@ -7,47 +9,59 @@ export function drawRightAside(artists, list){
 
         if(!artist) return;
 
-        // Clicked in delete icon
-        if(delIcon){
+        // Clicked in ? icon
+        if(delIcon && !list.length){
             ul.removeEventListener('click', listener);
-            return drawRightAside(artists, list.filter((a) => a != artist.dataset.artistid));
-        }
+            drawRightAside(artists, Object.keys(artists).sort(() => Math.random() - 0.6));
+        }else if(delIcon){ // Clicked in delete Icon
+            ul.removeEventListener('click', listener);
+            drawRightAside(artists, list.filter((a) => a != artist.dataset.artistid));
+        }else
 
         // Do something
         console.log(artists[artist.dataset.artistid]);
     };
 
-
-    // Ul drawing
     const ul = document.querySelector('#right-aside ul');
-    ul.innerHTML = list.slice(0, 8).map(key => {
-        /**
-         * TO-DO
-         * convert artists[key].nb_fan
-         *  actually can be 789512 followers
-         * but we need 789k followers instead
-         * and if the amount is up to a million
-         * we need 1M followers
-         * 
-        */
-        return `
-            <li class="mb-3" data-artistId="${key}">
-              <div class="flex grow center">
-                <div class="list-item-image">
-                    <img src="${artists[key].picture_medium}" alt="Profile ${artists[key].name}" height="100%" />
+
+    //Render out of recomendations state
+    if(!list.length){
+        ul.innerHTML =  `
+        <li class="mb-3" data-artistId="0">
+          <div class="flex grow center">
+            <div class="flex grow ml mb">
+                <p class="text text-primary">Do you like music at all</p>
+            </div>
+            <div class="flex center mb">
+                <span class="pointer text-body" style="color:var(--primary);">
+                <i class="fa-solid fa-question"></i>
+                </span>
+            </div>
+          </div>
+        </li>
+    `
+    }else{
+        // Ul drawing
+        ul.innerHTML = list.slice(0, 7).map(key => {
+            return `
+                <li class="mb-3" data-artistId="${key}">
+                <div class="flex grow center">
+                    <div class="list-item-image">
+                        <img src="${artists[key].picture_medium}" alt="Profile ${artists[key].name}" height="100%" />
+                    </div>
+                    <div class="flex grow column ml mb">
+                        <p class="text text-primary">${artists[key].name}</p>
+                        <p class="text-caption text-primary">${getTruncFans(artists[key].nb_fan)} followers</p>
+                    </div>
+                    <div class="flex center">
+                        <span class="pointer text text-primary">
+                        <i class="fa-solid fa-xmark"></i>
+                        </span>
+                    </div>
                 </div>
-                <div class="flex grow column ml mb">
-                    <p class="text text-primary">${artists[key].name}</p>
-                    <p class="text-caption text-primary">${artists[key].nb_fan} followers</p>
-                </div>
-                <div class="flex center">
-                    <span class="pointer text text-primary">
-                      <i class="fa-solid fa-xmark"></i>
-                    </span>
-                </div>
-              </div>
-            </li>
-        `
-    }).join('');
+                </li>
+            `
+        }).join('');
+    }
     ul.addEventListener('click', listener);
 }
