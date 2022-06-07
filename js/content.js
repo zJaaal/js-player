99a2407b7b08e06aa561a6300274861ccc325750
Diego Andrés <dandresfg@gmail.com>
2022-06-07 14:05:37 -0400

    fix: handleEvent

--- js/content.js
+++ js/content.js
@@ -1,47 +1,42 @@
 import { basicArtist } from "./utils";
 
 export const drawContent = (albums, artists) =>{
-    const content = document.querySelector("#content");
+  const content = document.querySelector("#content");
 
-    let currentArtist = 0;
-    let myAlbums = [];
-
-    albums.forEach((index) => {
-
-        index.data.data.slice(0,2).forEach((album) => {
-            album.artist = artists[basicArtist[currentArtist]];
-            myAlbums.push(album);
-        });
-        ++currentArtist;
+  const myAlbums = albums.reduce((acc, res, index) => {
+    res.data.data.slice(0,2).forEach((album) => {
+      album.artist = artists[basicArtist[index]];
+      acc.push(album);
     });
-    myAlbums.sort(()=> Math.random() - 0.6);
+    return acc;
+  }, [])
+  .sort(()=> Math.random() - 0.6)
 
-    content.innerHTML = myAlbums.map((album) => {
-        return `
-        <div class="flex column album-art" data-albumtitle="${album.title}" style="width: 150px;">
-        <img class="album-art-image" src="${album.cover_medium}" alt="${album.title}">
-        <div class="flex middle">
-          <span class="album-icon">
-            <i class="fa-solid fa-user text-select"></i>
-          </span>
-          <p class="text text-primary">${album.artist.name}</p>
-        </div>
-        <div class="flex">
-          <span class="album-icon">
-            <i class="fa-solid fa-compact-disc"></i>
-          </span>
-          <p class="text-caption text-primary">${album.title}</p>
-        </div>
-      </div>`
-    }).join("");
+  content.innerHTML = myAlbums.map((album) => {
+      return `
+      <div class="flex column album-art" data-albumtitle="${album.title}" style="width: 150px;">
+      <img class="album-art-image" src="${album.cover_medium}" alt="${album.title}">
+      <div class="flex middle">
+        <span class="album-icon">
+          <i class="fa-solid fa-user text-select"></i>
+        </span>
+        <p class="text text-primary">${album.artist.name}</p>
+      </div>
+      <div class="flex">
+        <span class="album-icon">
+          <i class="fa-solid fa-compact-disc"></i>
+        </span>
+        <p class="text-caption text-primary">${album.title}</p>
+      </div>
+    </div>`
+  }).join("");
 
-
-
-    function listener(evt){
-        const album = evt.path.find(path => path.tagName === "DIV");
-        if(!album.dataset.albumtitle) return;
-        console.log('You clicked in ', album.dataset.albumtitle);
-    };
-    content.addEventListener("click", listener);
-
+  function listener(evt){
+    const album = evt.path.find(path => path.tagName === "IMG");
+    if(!album){
+      return;
+    }
+    console.log('You clicked in ', album.dataset.albumtitle);
+  };
+  content.addEventListener("click", listener);
 }
