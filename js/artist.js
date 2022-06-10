3d3a8f73015f3c8cfef23b4675c9cb8d5bcab966
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-10 19:51:46 -0300

    Created drawArtist for dynamical drawing

--- js/artist.js
+++ js/artist.js
@@ -1,5 +1,9 @@
 import { getArtistInfo } from "./utils";
 
 export const drawArtist = async (id)=>{
-    console.log(await getArtistInfo(id));
+    const [artistInfo, topSongs, artistAlbums, relatedArtists] = await getArtistInfo(id); 
+    // console.log(artistInfo);
+    // console.log(topSongs);
+    console.log(artistAlbums);
+    console.log(relatedArtists);
 }
