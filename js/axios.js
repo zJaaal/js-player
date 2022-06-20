263f5c95e467bc46c747a9a221d97f6bb5848cb0
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-20 18:10:50 -0300

    Update axios.js
    Added enviroment variables

--- js/axios.js
+++ js/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import secrets from "../secrets.json";
 
 const instance = axios.create({
     baseURL: 'https://deezerdevs-deezer.p.rapidapi.com/',
     headers: {
-        'X-RapidAPI-Host': secrets.XRapidAPIHost,
-        'X-RapidAPI-Key': secrets.XRapidAPIKey
+        'X-RapidAPI-Host': process.env.XRapidAPIHost,
+        'X-RapidAPI-Key': process.env.XRapidAPIKey
     }
 });
 
