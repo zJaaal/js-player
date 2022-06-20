1e194ae500e87431a6b64aded88bc90f35f9da15
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-20 19:22:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import * as Utils from "./utils"
 
 (async function init(){
     try {
+        console.log(import.meta.env.KEY)
         const [ artists, albums ] = await Utils.getBasicData();
 
         // Draw
