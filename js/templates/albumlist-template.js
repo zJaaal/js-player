0eb23f68ad2b03dc9c20bafba30349743f4b1d6e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-20 17:56:39 -0300

    Added responsive design

--- js/templates/albumlist-template.js
+++ js/templates/albumlist-template.js
@@ -1,6 +1,6 @@
 
 export default (album) =>`
-<li class="flex column left album-art pointer" style="width: 170px;" data-albumid="${album.id}">
+<li class="flex column center album-art pointer" style="width: 170px;" data-albumid="${album.id}">
         <img class="album-art-image" src="${album.cover_medium}" alt="${album.title + " cover art"}">
     <div class="flex center">
         <p class="text text-wide text-primary">${album.title}</p>
